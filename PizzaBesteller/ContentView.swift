87bdf26181750437f56daeae36b031f5d8f8b613
Alexandre Samson <alexander_samson@icloud.com>
2025-05-09 22:28:15 +0200

    ...

--- PizzaBesteller/ContentView.swift
+++ PizzaBesteller/ContentView.swift
@@ -14,11 +14,30 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                Picker("Wähle Deine Pizza aus", selection: $pizza.pizzaTypeIndex) {
-                    ForEach(0..<Pizza.pizzaTypes.count, id: \.self) { index in// id: Добавил сам
-                        Text(Pizza.pizzaTypes[index]).tag(index)
+                Section {
+                    Picker("Wähle Deine Pizza aus", selection: $pizza.pizzaTypeIndex) {
+                        ForEach(0..<Pizza.pizzaTypes.count, id: \.self) { index in// id: Добавил сам
+                            Text(Pizza.pizzaTypes[index]).tag(index)
+                        }
+                    }
+                    Stepper(value: $pizza.anzahl, in: 1...4) {
+                        Text("Anzahl an Pizzen: \(pizza.anzahl)")
                     }
                 }
+                
+                Section {
+                    Picker("", selection: $pizza.pizzaSizeIndex) {
+                        ForEach(0..<Pizza.pizzaSize.count, id: \.self) {
+                            Text(Pizza.pizzaSize[$0]).tag($0)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Lieferung")) {
+                    Text("Lieferung...")
+                }
+                
             }
             .navigationBarTitle("Pizza Bestellen")
         }
