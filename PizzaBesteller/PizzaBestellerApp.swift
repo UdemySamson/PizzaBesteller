c0466e64aa035abdf3458e3355ed9d09a9507e05
Alexandre Samson <alexander_samson@icloud.com>
2025-05-09 20:11:49 +0200

    Initial Commit

---
+++ PizzaBesteller/PizzaBestellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaBestellerApp.swift
+//  PizzaBesteller
+//
+//  Created by Alexandre Samson on 09.05.25.
+//
+
+import SwiftUI
+
+@main
+struct PizzaBestellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
